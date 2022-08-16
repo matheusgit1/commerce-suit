f3a0d166c81aa67a5c0fcf30bd1b72f001c5b57e
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-08-15 23:43:00 -0300

    feat: add product service into project

---
+++ src/services/products-api/index.ts
@@ -0,0 +1,19 @@
+import { HttpClient, BasePaths } from '../../api'
+import { AxiosRequestHeaders } from 'axios'
+
+
+
+export class ProductApi {
+    public httpClient: HttpClient
+
+    constructor(){
+      this.httpClient = new HttpClient(new BasePaths().getBaseurlOfProductsApi())
+    }
+
+
+    public async getListProductWithLimit(limit: number){
+      const response = await this.httpClient.execute.get(`/list/limit/${limit}`)
+      return response
+    }
+    
+}
