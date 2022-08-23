e379f71c9524ea03de5f7fe2375ce1a1a625d73e
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-08-23 17:36:53 -0300

    feat: add product card component into project

--- src/components/ProductCards/product-card.tsx
+++ src/components/ProductCards/product-card.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import {
-  CardWrapper,
-  CardProductImage,
-  CardProductButtomContainer,
-  CardProductPrice,CardProductButtom,
-  CardProductDiscount
-} from './product-card.styles'
+import React from 'react';
 import { HiTag } from 'react-icons/hi'
 import { useProductContext } from '../../context'
+import { useWindowDimensions } from '../../hooks/useWindownDimension'
+import { Card, Modal, Button } from 'antd';
+import { ShoppingTwoTone, HeartTwoTone } from '@ant-design/icons'
+import { ProductDetails } from '..'
+import { useNavigate } from 'react-router-dom'
+import { paths } from '../../mocks/paths'
+
 interface IProductFeatures {
   title: string
   body: {
-      [x: string]: any
+    [x: string]: any
   }
 }
 
@@ -29,36 +29,98 @@ interface IProduct {
   conditions: string
   features: IProductFeatures[]
   isActive: boolean
+  seller: string
 }
 
-interface props{
+interface props {
   data: IProduct
 }
 
 
-export const ProductCards: React.FC<props> = ({data}) => {
+export const ProductCards: React.FC<props> = ({ data }) => {
+  // const { width } = useWindowDimensions()
+  const { Meta } = Card;
   const productContext = useProductContext()
-  React.useEffect(()=>{
+  const navigate = useNavigate()
+  React.useEffect(() => {
     // console.log("use: ", data)
     //do something
-  },[])
+  }, [])
 
   const addToWishList = async () => {
-     //to-do implementes wish to wish list
-     const response = await productContext.addToWishList(data.id)
-     return
+    const response = await productContext.addToWishList(data.id)
+    if (response) {
+      setInWishListColor('red')
+      return
+    }
+    setInWishListColor('gray')
   }
 
-  return(
+  const [isModalVisible, setIsModalVisible] = React.useState<boolean>(false)
+  const [InWishListColor, setInWishListColor] = React.useState<string>('gray')
+
+  React.useEffect(() => {
+
+  }, [InWishListColor])
+
+  const elipses = (): string => {
+    if (data.description.length > 27) {
+      return data.description.slice(0, 24) + '...'
+    }
+    return data.description
+  }
+  return (
     <React.Fragment>
-      <CardWrapper>
-        <CardProductImage src={data.images[0] ?? 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg' }/>
-        <CardProductPrice> R$ {data.price} </CardProductPrice>
-        <CardProductDiscount> {data.discount && +data.discount != 0 ? `-R$${data.discount} OFF` : ""}</CardProductDiscount>
-        <CardProductButtomContainer>
-          <CardProductButtom onClick={()=>addToWishList()}> Lista de desejos <HiTag color="#fff" size={25}/> </CardProductButtom>
-        </CardProductButtomContainer>
-      </CardWrapper>
+      <Modal
+        title={data.name.toUpperCase()}
+        centered
+        visible={isModalVisible}
+        onOk={() => setIsModalVisible(false)}
+        onCancel={() => setIsModalVisible(false)}
+        width={850}
+        style={{ padding: 0 }}
+        footer={[
+          <Button icon={<ShoppingTwoTone />} key="addToWishList" onClick={() => addToWishList()}>
+            Lista de desejos
+          </Button>,
+          <Button danger key="close" type="default" loading={false} onClick={() => setIsModalVisible(false)}>
+            Fechar
+          </Button>,
+          <Button
+            key="goTo"
+            loading={false}
+            onClick={() => {
+              setIsModalVisible(false)
+              window.scrollTo(0, 0)
+              navigate(`/produto/${data.id}`, { state: { data: data } })
+            }}
+          >
+            Ir para página de produto
+          </Button>,
+        ]}
+      >
+        <ProductDetails data={data} />
+      </Modal>
+
+      <Card
+        hoverable
+        style={{ width: 240, marginBottom: 30 }}
+        cover={<img onClick={() => setIsModalVisible(true)} style={{ height: 300, width: "100%" }} alt="example" src={data.images[0] ? data.images[0] : 'https://t4.ftcdn.net/jpg/04/99/93/31/360_F_499933117_ZAUBfv3P1HEOsZDrnkbNCt4jc3AodArl.jpg'} />}
+        actions={[
+          <HeartTwoTone
+            twoToneColor={InWishListColor}
+            onClick={() => addToWishList()}
+            style={{
+              fontSize: 20
+            }}
+            label="lista de desejos" />
+        ]}
+      >
+        <div onClick={() => setIsModalVisible(true)}>
+          <Meta title={data.name} description={elipses()} />
+          <Meta title={data.price} description={""} />
+        </div>
+      </Card>
     </React.Fragment>
   )
 }
