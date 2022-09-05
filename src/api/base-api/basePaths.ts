500842a4d127d2e63b3ea46a5f6ac5b44652d9a3
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-05 07:10:18 -0300

    feat: add base purchase api into project!

--- src/api/base-api/basePaths.ts
+++ src/api/base-api/basePaths.ts
@@ -20,4 +20,8 @@ export class BasePaths {
   public getBaseurlOfAdressApi(): IBasePath{
     return { basePath: 'http://127.0.0.1:3002/v1/adress', timeout: 15000 }
   }
+
+  public getBaseurlOfPurchaseApi(): IBasePath{
+    return { basePath: 'http://127.0.0.1:3004/v1/purchase', timeout: 15000 }
+  }
 }
