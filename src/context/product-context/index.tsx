971186e8c91c9ff980b25312d72099489a44526a
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-05 07:14:05 -0300

    feat: add new features and functions

--- src/context/product-context/index.tsx
+++ src/context/product-context/index.tsx
@@ -2,19 +2,32 @@ import React from 'react';
 import { AxiosRequestHeaders, AxiosPromise } from 'axios'
 import { IFilter } from '../../services'
 import { toast } from 'react-toastify'
-import { ProductApi } from '../../services'
+import { ProductApi, PurchaseApi } from '../../services'
+import { useAuthContext } from '../auth-context/useAuthContext'
 
 export type ProductContextType = {
   // user: user | undefined,
-  getListProductWithLimit: () => AxiosPromise
+  getListProductWithLimit: (limit: number) => AxiosPromise
   getListProductByCategory: (filter: IFilter) => AxiosPromise
-  addToWishList: (productId: string) => Promise<boolean>
+  addToWishList: (productId: string, token: string) => AxiosPromise
+  removeFromWishlist: (productId: string, token: string) => AxiosPromise
   getProductById: (productId: string) => AxiosPromise
+  addProductIntoCart: (productId: string, token: string) => AxiosPromise
   wishList: string[]
+  cartIds: IUserCart[]
 }
 
 export type ProductContextProvidersProps = {
   children: React.ReactNode,
+}
+
+interface IUserCart {
+  id: string
+  co_product_id: string
+  co_user_id: string
+  co_created_at: Date
+  co_updated_at: Date
+  co_quantity: number
 }
 
 export const ProductContext = React.createContext({} as ProductContextType);
@@ -22,17 +35,50 @@ export const ProductContext = React.createContext({} as ProductContextType);
 export function ProductContextProvider(props: ProductContextProvidersProps) {
   const USER_IN_LOCAL_STORAGE = "commerce-suit-user"
   const productApi = new ProductApi()
-  React.useEffect(() => {
-    const initialize = () => {
+  const purchaseApi = new PurchaseApi()
 
-    }
-    initialize()
-  }, [])
+  const authContext = useAuthContext()
 
   const [wishList, setWishList] = React.useState<string[]>([])
+  const [cartIds, setCartIds] = React.useState<IUserCart[]>([])
 
-  const getListProductWithLimit = async () => {
-    const response = await productApi.getListProductWithLimit(10)
+  //get all in wish list
+  React.useEffect(() => {
+    const initialize = async () => {
+      if (!authContext.user) return
+      try {
+        const { data } = await purchaseApi.listWishlist(authContext.user?.access_token || "")
+        for (const index in data) {
+          setWishList(oldArray => [...oldArray, data[index]])
+        }
+      } catch (error: any) {
+        //do somethnig
+      }
+    }
+    initialize()
+  }, [authContext.user])
+
+
+  //get all products in cart
+  React.useEffect(() => {
+    const initialize = async () => {
+      if (!authContext.user) return
+      try {
+        const { data } = await purchaseApi.getUserCart(authContext.user?.access_token || "")
+        // console.log(data)
+        for (const index in data) {
+          console.log(data[index]?.co_product_id)
+          setCartIds(oldArray => [...oldArray, data[index]?.co_product_id])
+        }
+      } catch (error: any) {
+        //do somethnig
+      }
+    }
+    initialize()
+  }, [authContext.user])
+
+  const getListProductWithLimit = async (limit: number) => {
+    const response = await productApi.getListProductWithLimit(limit)
     return response
   }
   const getListProductByCategory = async (
@@ -41,15 +87,16 @@ export function ProductContextProvider(props: ProductContextProvidersProps) {
     return response
   }
 
-  const addToWishList = async (productId: string): Promise<boolean> => {
-    if (wishList.includes(productId)) {
-      setWishList(wishList.filter(item => item !== productId));
-      toast.warn("[simulação] - removido da lista de desejos")
-      return false
-    }
-    toast.success("[simulação] - adiconado a lista de desejos")
-    wishList.push(productId)
-    return true
+  const addToWishList = async (productId: string, token: string) => {
+    const response = await purchaseApi.insertInWishList(productId, token)
+    setWishList(oldArray => [...oldArray, productId])
+    return response
+  }
+
+  const removeFromWishlist = async (productId: string, token: string) => {
+    const response = await purchaseApi.removeFromWishlist(productId, token)
+    setWishList(wishList.filter(productIdContent => productIdContent !== productId));
+    return response
   }
 
   const getProductById = async (productId: string) => {
@@ -57,15 +104,24 @@ export function ProductContextProvider(props: ProductContextProvidersProps) {
     return response
   }
 
+  const addProductIntoCart = async (productId: string, token: string) => {
+    const response = await purchaseApi.addProductIntoCart(productId, token)
+    return response
+  }
+
 
   return (
-    <ProductContext.Provider value={{
-      getListProductWithLimit,
-      addToWishList,
-      getListProductByCategory,
-      getProductById,
-      wishList
-    }}
+    <ProductContext.Provider
+      value={{
+        getListProductWithLimit,
+        addToWishList,
+        removeFromWishlist,
+        getListProductByCategory,
+        getProductById,
+        addProductIntoCart,
+        wishList,
+        cartIds
+      }}
     >
       {props.children}
     </ProductContext.Provider>
