5e41133c9b7f2d2a6414679a62108c99536f4836
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-05 07:18:51 -0300

    feat: add new routes

--- src/routes/routing.tsx
+++ src/routes/routing.tsx
@@ -6,11 +6,16 @@ import {
 } from 'react-router-dom'
 
 import {
+  Login,
+  Register,
+  VerifyAcount,
+  ResetPassword,
+  ChangePasswordWithoutLogin,
   Home,
-  WishList,
+  ProductById,
+  NotFound,
   MyPurchases
 } from '../pages'
-
 import { paths } from '../mocks/paths'
 import { ToastContainer } from 'react-toastify';
 import { AuthRoutes } from './authRoutes'
@@ -29,14 +34,17 @@ export const Routing = () => {
   return (
     <React.Fragment>
       <Router>
-        <AuthRoutes />
-        {
-          !authRoutes.includes(window.location.pathname) && <Header />
-        }
+        <Header />
         <Routes>
-          <Route path={paths.home} element={<Home />} />
-          <Route path={paths.wishList} element={<WishList />} />
-          <Route path={paths.myPurchases} element={<MyPurchases />} />
+          <Route path={paths.login} element={<Login />} />
+          <Route path={paths.newAcount} element={<Register />} />
+          <Route path={paths.verifyAcount} element={<VerifyAcount />} />
+          <Route path={paths.resetPassword} element={<ResetPassword />} />
+          <Route path={paths.changePasswordWithoutLogin} element={<ChangePasswordWithoutLogin />} />
+          <Route path={paths.productById} element={<ProductById />} />
+          <Route path={paths.home || ""} element={<Home />} />
+          <Route path={paths.myPurchases || ""} element={<MyPurchases />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         <ToastContainer />
       </Router>
