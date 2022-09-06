167dada03ebae8e10514738e11766f035042983f
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-05 22:59:34 -0300

    feat: add mockto personaliza header

---
+++ src/components/Header/Mocks/notification.mock.ts
@@ -0,0 +1,28 @@
+interface INotificationMock {
+  title: string
+  body: string
+}
+
+export const notification: INotificationMock[] = [
+  {
+    title: "notificação 1",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    title: "notificação 2",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    title: "notificação 3",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    title: "notificação 4",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    title: "notificação 5",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+]
+  
---
+++ src/components/Header/Mocks/questions.mock.ts
@@ -0,0 +1,18 @@
+interface INotificationMock {
+  body: string
+}
+
+export const questions: INotificationMock[] = [
+  {
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+  {
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas facilisis pellentesque ligula, quis maximus massa malesuada nec. Etiam nec sodales dui."
+  },
+]
