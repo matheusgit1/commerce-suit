1a1c3b6b3e62e4c190a30d5fc7091610d8234c19
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-11 18:59:51 -0300

    feat: add a global loading

---
+++ src/components/GlobalLoadingPage/GlobalLoadingPage.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Spin, Result, Typography } from 'antd'
+import { AppstoreTwoTone } from '@ant-design/icons'
+
+export const GlobalLoadingPage: React.FC = () => {
+  return (
+    <React.Fragment>
+      <div style={{ display: "flex", flex: 1, height: "100%", justifyContent: "center", alignItems: "center" }}>
+        <Result
+          icon={<AppstoreTwoTone spin width={200} />}
+          title={<Typography.Title level={4} style={{ opacity: 0.6 }}>Carregando...</Typography.Title>}
+        />
+      </div>
+    </React.Fragment>
+  )
+}
