ab4e47db40e1f08fdf2fbe0b136df344f410ba80
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-11 20:13:07 -0300

    feat: add new component into project

---
+++ src/components/WithoutContent/WithoutContent.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Result } from 'antd'
+
+export const WithoutContent: React.FC = () => {
+  return (
+    <React.Fragment>
+      <Result
+        status="404"
+        // title="404"
+        subTitle="Nada para mostrar nessa página."
+      />
+    </React.Fragment>
+  )
+}
