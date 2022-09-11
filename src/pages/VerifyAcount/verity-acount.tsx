f6873f5de183f1b6b41bdd9906cf28ddd8b308db
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-11 15:31:03 -0300

    fix: change toast by message from antd

--- src/pages/VerifyAcount/verity-acount.tsx
+++ src/pages/VerifyAcount/verity-acount.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {Container, Title} from './verify-acount.styles'
+import { message } from 'antd'
+import { Container, Title } from './verify-acount.styles'
 import { Form } from '../../components'
 import { useNavigate } from 'react-router-dom'
-import {paths} from '../../mocks/paths'
-import {useLocation} from 'react-router-dom';
+import { paths } from '../../mocks/paths'
+import { useLocation } from 'react-router-dom';
 import { AuthApi } from '../../services/auth-api'
 import { useAuthContext } from '../../context'
-import {  toast } from 'react-toastify';
 
 
-interface props {}
+interface props { }
 
-export const VerifyAcount: React.FC<props> = ({}) => {
+export const VerifyAcount: React.FC<props> = ({ }) => {
 
   const location = useLocation()
   const AuthContext = useAuthContext()
@@ -32,37 +32,37 @@ export const VerifyAcount: React.FC<props> = ({}) => {
 
   const onSubmit = async (event: any) => {
     event.preventDefault()
-    try{
-      const response = await AuthContext.verifyAcount({email, verifyCode})
-      toast.success(response.data.mensagem)
-      navigate(paths.login, {state:{email: email}})
+    try {
+      const response = await AuthContext.verifyAcount({ email, verifyCode })
+      message.success(response.data.mensagem)
+      navigate(paths.login, { state: { email: email } })
       return;
-    }catch(error: any){
-      if(error.response.data.erro){
-        toast.error(error.response.data.erro)
+    } catch (error: any) {
+      if (error.response.data.erro) {
+        message.error(error.response.data.erro)
         return
       }
-      toast.error("Erro interno")
+      message.error("Erro interno")
       return
     }
   }
 
   const resentVerifyCode = async () => {
-    if(email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
-      try{
-        const response = await AuthContext.resentVerifyCode({email})
-        toast.success(response.data.mensagem)
+    if (email.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+      try {
+        const response = await AuthContext.resentVerifyCode({ email })
+        message.success(response.data.mensagem)
         return
-      }catch(error: any){
-        if(error.response.data.erro){
-          toast.error(error.response.data.erro)
+      } catch (error: any) {
+        if (error.response.data.erro) {
+          message.error(error.response.data.erro)
           return
         }
-        toast.error("Erro interno")
+        message.error("Erro interno")
         return
       }
     }
-    toast.error("Email inválido")
+    message.error("Email inválido")
     return
   }
 
@@ -70,7 +70,7 @@ export const VerifyAcount: React.FC<props> = ({}) => {
   const [email, setEmail] = React.useState<string>(location.state?.email || '')
   const [verifyCode, setVeirfyCode] = React.useState<string>('')
 
-  return(
+  return (
     <React.Fragment>
 
       <Container>
@@ -84,12 +84,12 @@ export const VerifyAcount: React.FC<props> = ({}) => {
             <FormBody onSubmit={onSubmit}>
               <FormFieldset>
                 <FormFieldLabel>E-mail</FormFieldLabel>
-                <FormInput defaultValue={email}  placeholder="E-mail" type="email" required onChange={(e)=>setEmail(e.target.value)}/>
+                <FormInput defaultValue={email} placeholder="E-mail" type="email" required onChange={(e) => setEmail(e.target.value)} />
               </FormFieldset>
 
               <FormFieldset>
                 <FormFieldLabel>Código de verificação</FormFieldLabel>
-                <FormInput placeholder="codigo de verificação" type="text" required onChange={(e)=>setVeirfyCode(e.target.value)} />
+                <FormInput placeholder="codigo de verificação" type="text" required onChange={(e) => setVeirfyCode(e.target.value)} />
               </FormFieldset>
 
               <FormFieldset>
@@ -97,11 +97,11 @@ export const VerifyAcount: React.FC<props> = ({}) => {
               </FormFieldset>
 
               <FormFieldset>
-                <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
-                  <FormLink onClick={()=>navigate(paths.login)}>ou faça login</FormLink>
-                  <FormLink onClick={()=>resentVerifyCode()} style={{marginTop: 20}}>reenviar código</FormLink>
+                <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                  <FormLink onClick={() => navigate(paths.login)}>ou faça login</FormLink>
+                  <FormLink onClick={() => resentVerifyCode()} style={{ marginTop: 20 }}>reenviar código</FormLink>
                 </div>
-               
+
               </FormFieldset>
             </FormBody>
           </FormWrapper>
