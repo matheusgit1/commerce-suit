09a36613bf3eb81f8cea1be9a90632774422eb42
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-11 15:34:56 -0300

    fix: remove toast css from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { AuthContextProvider, ProductContextProvider } from './context'
 import { Routing } from './routes'
 import 'antd/dist/antd.css';
-import 'react-toastify/dist/ReactToastify.css';
 
 
 function App() {
