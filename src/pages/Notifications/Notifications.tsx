219a757a0f5cab890bf6823fe9e7b7b2b7766b14
Matheus Alves Pereira <pereira.matheusalves@gmail.com>
2022-09-11 20:21:44 -0300

    feat: add inital structure to page of notifies and questions

---
+++ src/pages/Notifications/Notifications.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { } from 'antd'
+
+export default function Notifications({ }) {
+  return (
+    <React.Fragment>
+      <h1>notificação</h1>
+    </React.Fragment>
+  )
+}
---
+++ src/pages/Questions/Questions.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { } from 'antd'
+
+export default function Questions({ }) {
+  return (
+    <React.Fragment>
+      <h1>Questions</h1>
+    </React.Fragment>
+  )
+}
